1e96b55ae4670f5eeddac87f84ea230924bbd1f6
batatavadaX <75261987+batatavadaX@users.noreply.github.com>
2020-12-19 00:02:15 +0530

    Update start.py

--- start.py
+++ start.py
@@ -4,4 +4,4 @@ sys.path.insert(1, 'mutation')
 from music import jam
 
 if __name__ == "__main__":
-	jam.run(host="0.0.0.0",port=variables.PORT,debug=True)
+	jam.run(host="0.0.0.0", port=variables.PORT, debug=True)
