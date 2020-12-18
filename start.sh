be8f2e7ab5d7e1b2544c68c7b40f843e92063de3
batatavadaX <75261987+batatavadaX@users.noreply.github.com>
2020-12-18 23:24:42 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,12 +3,5 @@
 wget 'https://openresty.org/download/nginx-1.17.8.tar.gz'
 tar -zxvf nginx-1.17.8.tar.gz
 
-cd /usr/local/bin
-mkdir ffmpeg
-
-wget 'http://johnvansickle.com/ffmpeg/releases/ffmpeg-release-64bit-static.tar.xz'
-tar xf ffmpeg-release-64bit-static.tar.xz
-mv * ../
-
 chmod 777 start.py 
 python start.py
