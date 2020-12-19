48ca819907caf7b5ef43e16a236eebd0336fa8fa
batatavadaX <75261987+batatavadaX@users.noreply.github.com>
2020-12-19 15:43:40 +0530

    Update music.py

--- mutation/music.py
+++ mutation/music.py
@@ -1,15 +1,19 @@
 import os
 from flask import Flask, render_template
-
+from flask import send_file, send_from_directory, safe_join, abort
 jam = Flask(__name__)
 
 @jam.route("/")
 def home_():
   return render_template("home.html")
 
-@jam.route("/stream", methods=["GET"])
-def stream():
-  if not os.path.exists("stream"):
-    os.mkdir("stream")
-  for filename in os.listdir("stream"):
-    sys.path.append("stream")
+@jam.route("/stream/<stream_name>")
+x = glob.iglob('templates/stream/*.mp3', recursive=True)
+for mp3 in x:
+  a = print(mp3)
+  return "mp3 files"
+  return send_file(a, mimetype='audio/mpeg')
+
+
+
+
